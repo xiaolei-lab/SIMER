9254a8f7fa90790efbe1cd67cddec49781dcdb04
yindong <yindong012@163.com>
2019-12-07 19:44:38 +0800

    - update parameters

--- R/simer.r
+++ R/simer.r
@@ -66,7 +66,7 @@
 #' @param prog.back a vector with litter size in every generation of back-cross
 #' @param ps fraction selected in selection
 #' @param decr whether to sort by descreasing
-#' @param sel.multi selection method of multi-trait with options: "tdm", "indcul" and "index"
+#' @param sel.multi selection method of multiple traits with options: "tdm", "indcul" and "index"
 #' @param index.wt economic weights of selection index method
 #' @param index.tdm index represents which trait is being selected. NOT CONTROL BY USER
 #' @param goal.perc percentage of goal more than mean of scores of individuals
