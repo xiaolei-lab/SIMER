235f57d0d5f7873849e41fe47b30555a15c0f71f
yindong <yindong012@163.com>
2022-08-28 11:41:53 +0800

    - update simer.Genotype.r

--- R/simer.Genotype.r
+++ R/simer.Genotype.r
@@ -142,8 +142,8 @@ genotype <- function(SP = NULL, ncpus = 0, verbose = TRUE) {
       snp.index <- (1:pop.marker)[-qtn.index]
       num.mut.qtn <- ceiling(spot.total * rate.mut[[1]])
       num.mut.snp <- ceiling(spot.total * rate.mut[[2]])
-      row.mut <- c(sample(qtn.index, num.mut.qtn), sample(qtn.index, num.mut.qtn))
-      col.mut <- sample(1:(incols*pop.ind), num.mut.qtn+num.mut.snp)
+      row.mut <- c(sample(qtn.index, num.mut.qtn), sample(snp.index, num.mut.snp))
+      col.mut <- sample(1:(incols*pop.ind), num.mut.qtn+num.mut.snp, replace = TRUE)
       for (j in 1:length(row.mut)) {
         if (bigmat[row.mut[j], col.mut[j]] == 1) {
           bigmat[row.mut[j], col.mut[j]] <- 0
