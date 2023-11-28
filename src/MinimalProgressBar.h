1f695a8318f2666fe9e196ef9f3b4357c374c234
yindong <yindong012@163.com>
2023-11-28 11:30:26 +0800

    - fix CRAN WARN

--- src/MinimalProgressBar.h
+++ src/MinimalProgressBar.h
@@ -12,26 +12,29 @@ using namespace Rcpp;
 
 class MinimalProgressBar: public ProgressBar{
   public:
-  MinimalProgressBar()  {
+  MinimalProgressBar(const string str = "Calculating in process")  {
     _finalized = false;
+    _str = str;
   }
   ~MinimalProgressBar() {}
   void display() {}
   void update(float progress) {
     if (_finalized) return;
     REprintf("\r");
-    REprintf(" Calculating in process...(finished %.2f%)", progress * 100);
+    REprintf(_str.c_str());
+    REprintf("...finished %u%%", (int)(progress * 100));
   }
   void end_display() {
     if (_finalized) return;
     REprintf("\r");
-    
-    REprintf(" Calculating in process...(finished 100.00%)");
+    REprintf(_str.c_str());
+    REprintf("...[finished 100%%]");
     REprintf("\n");
     _finalized = true;
   }
   private:
   bool _finalized;
+  string _str;
 };
 
 #endif
