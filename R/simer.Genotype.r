3c287c6113537c0a92dd7e574e2b48ad1dbcc06e
yindong <yindong012@163.com>
2024-07-10 17:30:45 +0800

    - update simer.Genotype.r

--- R/simer.Genotype.r
+++ R/simer.Genotype.r
@@ -355,7 +355,7 @@ annotation <- function(SP, verbose = TRUE) {
       if (is.null(pop.map$QTNProb)) {
         qtn.trn[[i]] <- sort(sample(1:nrow(pop.map), sum(qtn.num[[i]])))
       } else {
-        qtn.trn[[i]] <- sort(sample(1:nrow(pop.map), sum(qtn.num[[i]])), prob = pop.map$QTNProb)
+        qtn.trn[[i]] <- sort(sample(1:nrow(pop.map), sum(qtn.num[[i]]), prob = pop.map$QTNProb))
       }
       logging.log(" Number of selected markers of trait", i, ":", qtn.num[[i]], "\n", verbose = verbose)
     }
