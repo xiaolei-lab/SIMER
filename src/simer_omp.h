023aee9a88268290108a762854d1b31ff8fd62bb
yindong <yindong012@163.com>
2024-12-27 11:59:41 +0800

    - update simer_omp.h

--- src/simer_omp.h
+++ src/simer_omp.h
@@ -1,14 +1,26 @@
+// Copyright (C) 2016-2019 by Xiaolei Team
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef SIMER_OMP_H_
 #define SIMER_OMP_H_
 
 #if defined(_OPENMP)
 #include <omp.h>
-// [[Rcpp::plugins(openmp)]]
-#else
 #endif
 
 #include <Rcpp.h>
-// [[Rcpp::plugins(cpp11)]]
+
 static int omp_setup(int threads);
 static inline int omp_setup(int threads=0) {
     int t = 1;
